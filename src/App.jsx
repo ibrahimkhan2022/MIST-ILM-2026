69d1b092cfd86a5e05b1ee03adc40abd7c21800f
ibrahimkhan2022 <1khanibrahim2022@gmail.com>
2026-04-29 19:13:14 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Ilm</h1>
+      <p>Understand the Khutbah. Revisit the Message.</p>
+      <p>A web app to help Muslims save, review, and understand Jumu'ah khutbahs.</p>
+      <button onClick={() => alert('Coming soon!')}>Get Started</button>
+    </div>
+  );
+}
