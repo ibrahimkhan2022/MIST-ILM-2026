ff95fc1371848e01e8ee26b78b16984e8da4f576
ibrahimkhan2022 <1khanibrahim2022@gmail.com>
2026-05-03 20:55:29 -0400

    added to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Repo name on GitHub: MIST-ILM-2026
+// Site will be served at https://<user>.github.io/MIST-ILM-2026/
 export default defineConfig({
   plugins: [react()],
+  base: '/MIST-ILM-2026/',
   server: {
     port: 5173,
     open: true,
